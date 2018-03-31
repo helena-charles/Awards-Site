58645bc5d8319928d1a86fdd490d409f1657df7d
Katie Moore <kemoore1431@gmail.com>
2018-03-31 17:51:24 +0100

    added auth controller, user model and auth routes

--- config/router.js
+++ config/router.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const auth = require('./controllers/auth');
+
+router.post('/login', auth.login);
+router.post('/register', auth.register);
 
 router.route('/*')
   .all((req, res) => res.status(404).json({ message: 'Not found' }));
