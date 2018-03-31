32d449d453ba8e35e6a887e88a5934647b18528a
Helena Charles <helsz@hotmail.co.uk>
2018-03-31 18:13:38 +0100

    starting backend, index route working for questions

--- config/router.js
+++ config/router.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const questions = require('../controllers/questions');
+
+router.route('/questions')
+  .get(questions.index);
 
 router.route('/*')
   .all((req, res) => res.status(404).json({ message: 'Not found' }));
--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 const router = require('./config/router');
+
 const { dbURI, port } = require('./config/environment');
+
 const app = express();
+app.use(express.static(`${__dirname}/public`));
 
 mongoose.connect(dbURI);
 app.use(bodyParser.json());
 
-app.use(express.static(`${__dirname}/public`));
-
 app.use('/api', router);
-app.use('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.use((err, req,res,next) => {
-  if (err.name === 'ValidationError') {
 
-    const message = [];
-    for(const key in err.errors){
-      message.push(err.errors[key].message);
-    }
-    res.status(422).json({ message: message.join(' & ') }); // this is used in controllers/register.js
-
-  }
+  if (err.name === 'ValidationError') res.status(422).json({ message: err.message });
   res.status(500).json({ message: 'Internal Server Error '});
   next();
 });
 
-app.listen(port, `Up and running on port ${port}`);
+app.listen(port, () => console.log(`Aye aye captain, pulling into port ${port}`));
 
 module.exports = app;
