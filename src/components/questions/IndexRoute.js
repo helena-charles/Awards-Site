f41a4fd275537b1c4778ba92dd39b47deafa5e30
Helena Charles <helsz@hotmail.co.uk>
2018-04-16 19:00:55 +0100

    added card for questions, only show question if moderated, and added basic input with console log for voting

--- src/components/questions/IndexRoute.js
+++ src/components/questions/IndexRoute.js
@@ -12,19 +12,38 @@ class IndexRoute extends React.Component {
       .then(res => this.setState({ questions: res.data }));
   }
 
+  handleVote = (e) => {
+    console.log(e.target.value);
+  }
+
   render() {
     return (
-      <ul className="columns is-multiline">
-        {this.state.questions.map((question, i) =>
-          <li key={i} className="column is-one-third">
-            <div className="card">
-              <div className="card-content">
-                <h1 className="title is-4">{question.question}</h1>
-              </div>
-            </div>
-          </li>
-        )}
-      </ul>
+      <section>
+        <ul className="columns is-multiline">
+          {this.state.questions.map((question, i) =>
+            <li key={i} className="column is-one-third">
+              {question.moderated &&
+                <div>
+                  <div className="card">
+                    <div className="card-content">
+                      <h1 className="title is-4">{question.question}</h1>
+                    </div>
+                  </div>
+                  <div>
+                    <select onChange={this.handleVote}>
+                      <option value="Helena">Helena</option>
+                      <option value="Katie">Katie</option>
+                      <option value="Jess">Jess</option>
+                      <option value="Abi">Abi</option>
+                    </select>
+                    <button>Vote</button>
+                  </div>
+                </div>
+              }
+            </li>
+          )}
+        </ul>
+      </section>
     );
   }
 }
