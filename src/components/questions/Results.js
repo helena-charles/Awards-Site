b62666966579b66ab172c8e94bb4fc2022e121e0
Katie Moore <kemoore1431@gmail.com>
2018-04-19 21:16:18 +0100

    changing image file paths

--- src/components/questions/Results.js
+++ src/components/questions/Results.js
@@ -67,22 +67,22 @@ class Results extends React.Component {
 
   render() {
     const mates = {
-      'Reena': '/assets/images/reena.jpg',
-      'Tom': '/assets/images/thomas.jpg',
-      'George': '/assets/images/george.jpg',
-      'Jess': '/assets/images/jess.jpg',
-      'Katie': '/assets/images/katie.jpg',
-      'Aimee': '/assets/images/aimee.jpg',
-      'Fabian': '/assets/images/fabian.jpg',
-      'Abi': '/assets/images/abi.jpg',
-      'Sui': '/assets/images/sui.jpg',
-      'Nick': '/assets/images/nick.jpg',
-      'Mark': '/assets/images/mark.jpg',
-      'Mike': '/assets/images/mike.jpg',
-      'Helena': '/assets/images/helena.jpg',
-      'Fabienne': '/assets/images/fabienne.jpg',
-      'Paula': '/assets/images/paula.jpg',
-      'Amir': '/assets/images/amir.jpg'
+      'Reena': '../assets/images/reena.jpg',
+      'Tom': '../assets/images/thomas.jpg',
+      'George': '../assets/images/george.jpg',
+      'Jess': '../assets/images/jess.jpg',
+      'Katie': '../assets/images/katie.jpg',
+      'Aimee': '../assets/images/aimee.jpg',
+      'Fabian': '../assets/images/fabian.jpg',
+      'Abi': '../assets/images/abi.jpg',
+      'Sui': '../assets/images/sui.jpg',
+      'Nick': '../assets/images/nick.jpg',
+      'Mark': '../assets/images/mark.jpg',
+      'Mike': '../assets/images/mike.jpg',
+      'Helena': '../assets/images/helena.jpg',
+      'Fabienne': '../assets/images/fabienne.jpg',
+      'Paula': '../assets/images/paula.jpg',
+      'Amir': '../assets/images/amir.jpg'
     };
     return (
       <section>
